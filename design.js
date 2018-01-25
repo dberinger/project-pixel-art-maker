be7647cdf59e1df456c4b73cb009bcee0ff6f0b9
dberinger <danielberinger95@gmail.com>
2018-01-25 18:27:31 +0100

    refactor: improved functions, cleaner HTML

--- design.js
+++ design.js
@@ -1,59 +1,75 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
-
-//NOTE: no while loop since Forum Mentors said it's not a requirement
+//NOTE: no while loop since Forum Mentors said it's not a requirement;
+// using .append in loops slows down performance tremendously from what I've read
 
 const myTable = $(".grid");
+const myForm = document.querySelector("form");
 
-function addRows(table, n) {
-
-    var i = 0;
-    for (i; i < n; i++) {
-        table.append("<tr></tr>");
-    }
+function insertRow(tableID) {
+    tableID.each(function () {
+        var $table = $(this);
+        // Number of tds in the last table row
+        var n = $('tr:last td', this).length;
+        var tds = '<tr>';
+        for (var i = 0; i < n; i++) {
+            tds += '<td></td>';
+        }
+        tds += '</tr>';
+        if ($('tbody', this).length > 0) {
+            $('tbody', this).append(tds);
+        } else {
+            $(this).append(tds);
+        }
+    });
 };
 
-function addCells(table, n) {
-    var k = 0;
-    for (k; k < n; k++) {
-        $("tr").append("<td></td>");
-    }
+function insertColumn(tableID) {
+    tableID.find("tr").append("<td></td>");
 };
 
-//TODO: add insertRow function to enable user to add a single row to the created grid (table)
-
-function insertRow() {
-    addRows(myTable,1);
-    addCells(myTable,parseInt(10));
+function removeRow(tableID) {
+    tableID.find("tr:last-child").remove();
 };
 
-function insertColumn() {
-    $("tr").append("<td></td>");
-};
-
-function removeRow() {
-    $("tr:last-child").remove();
-};
-
-function removeColumn() {
-    $("td:last-child").remove();
+function removeColumn(tableID) {
+    tableID.find("td:last-child").remove();
 };
 
 function myGrid() {
     let width = parseInt($("#width").val());
     let height = parseInt($("#height").val());
-
+    let tableStr = "";
     myTable.empty();
-    addRows(myTable, height);
-    addCells(myTable, width);
+    // Create rows and colums;
+    for (var i = 0; i < height; i++) {
+        tableStr += "<tr>";
+        for (var j = 0; j < width; j++) {
+            tableStr += "<td></td>";
+        }
+        tableStr += "</tr>";
+    }
+    myTable.append(tableStr);
 };
 
 //TODO: show extra buttons only after submitting
 
-const myForm = document.querySelector("form");
 myForm.addEventListener('submit', function (e) {
     e.preventDefault();
     myGrid();
+});
+
+$("#add-row-btn").click(function () {
+    insertRow(myTable);
+});
+$("#del-row-btn").click(function(){
+    removeRow(myTable);
+})
+$("#add-col-btn").click(function(){
+    insertColumn(myTable);
+});
+$("#del-col-btn").click(function () {
+    removeColumn(myTable);
 });
 
 myTable.on("click", "td", function () {
