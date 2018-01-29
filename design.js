46e0afaed41247b6a13c8317a006d10bb126d36b
dberinger <danielberinger95@gmail.com>
2018-01-29 16:48:11 +0100

    refactor:  extra comments, styling fixes

--- design.js
+++ design.js
@@ -1,23 +1,30 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
 //NOTE: no while loop since Forum Mentors said it's not a requirement;
-// using .append in loops slows down performance tremendously from what I've read
+// using .append in loops slows down performance tremendously from what I've read :x
 
 const myTable = $(".grid");
 const myForm = document.querySelector("form");
+const buttons = $(".buttons");
+//boolean used in drawing functionalities
+let isHolding;
+//by default hides extra buttons adjusting grid before submition
+buttons.hide();
+
+/*  GRID CREATION FUNCTIONS     */
 
 function insertRow(tableID) {
     tableID.each(function () {
         var $table = $(this);
         // Number of tds in the last table row
-        var n = $('tr:last td', this).length;
-        var tds = '<tr>';
+        var n = $("tr:last td", this).length;
+        var tds = "<tr>";
         for (var i = 0; i < n; i++) {
-            tds += '<td></td>';
+            tds += "<td></td>";
         }
-        tds += '</tr>';
-        if ($('tbody', this).length > 0) {
-            $('tbody', this).append(tds);
+        tds += "</tr>";
+        if ($("tbody", this).length > 0) {
+            $("tbody", this).append(tds);
         } else {
             $(this).append(tds);
         }
@@ -36,12 +43,8 @@ function removeColumn(tableID) {
     tableID.find("td:last-child").remove();
 };
 
-function myGrid() {
-    let width = parseInt($("#width").val());
-    let height = parseInt($("#height").val());
+function createTableStr(height, width) {
     let tableStr = "";
-    myTable.empty();
-    // Create rows and colums;
     for (var i = 0; i < height; i++) {
         tableStr += "<tr>";
         for (var j = 0; j < width; j++) {
@@ -49,38 +52,73 @@ function myGrid() {
         }
         tableStr += "</tr>";
     }
-    myTable.append(tableStr);
+    return tableStr;
+}
+
+function myGrid() {
+    let width = parseInt($("#width").val());
+    let height = parseInt($("#height").val());
+    myTable.empty();
+    myTable.append(createTableStr(height, width));
 };
 
-//TODO: show extra buttons only after submitting
+/*   SUBMIT EVENT    */
 
-myForm.addEventListener('submit', function (e) {
+myForm.addEventListener("submit", function (e) {
+    //prevent page from refreshing
     e.preventDefault();
+    //invoke grid creating function
     myGrid();
+    //show extra grid adjusting buttons since now grid exists
+    buttons.show();
 });
+
+/*   BUTTONS CLICK EVENTS    */
 
 $("#add-row-btn").click(function () {
     insertRow(myTable);
 });
-$("#del-row-btn").click(function(){
+$("#del-row-btn").click(function () {
     removeRow(myTable);
 })
-$("#add-col-btn").click(function(){
+$("#add-col-btn").click(function () {
     insertColumn(myTable);
 });
 $("#del-col-btn").click(function () {
     removeColumn(myTable);
 });
 
+/*   DRAWING FUNCTIONALITIES     */
+
+//fill cell with picked color on click
 myTable.on("click", "td", function () {
     $(this).css("background-color", $("#color").val());
 });
 
-//TODO: improve coloring while holding with mouse + doesn't work on mobile
-myTable.on("dragover", "td", function () {
-    $(this).css("background-color", $("#color").val());
+//clear cell color on double click
+myTable.on("dblclick", "td", function () {
+    $(this).css("background-color", "");
 });
 
-myTable.on("dblclick", "td", function () {
-    this.css("background-color", "");
+//sets true that user is holding a mouse button down
+myTable.on("mousedown", "td", function (e) {
+    /*prevent default because it caused a bug:
+    when held click was more of a drag like action,
+    it turned to a not-allowed cursor causing the mouse to
+    draw infinetely across canvas until user clicked again;
+    behavior haven't occured since adding the line below'*/
+    e.preventDefault();
+    isHolding = true;
+});
+
+//while user holds a button cells over which mouse moves will be colored
+myTable.on("mousemove", "td", function () {
+    if (isHolding) {
+        $(this).css("background-color", $("#color").val());
+    }
+});
+
+//sets false as user let go
+myTable.on("mouseup", "td", function () {
+    isHolding = false;
 });
